e689caaf0e60a0d6c6eac71621a15a299970a56d
Ray Chen <53246211+Raychanan@users.noreply.github.com>
2023-05-20 23:55:46 -0500

    Incorporate XML API requests into a custom-built XML parser I created.

--- tests/parse-tei.py
+++ tests/parse-tei.py
@@ -1,12 +1,30 @@
 import os
 import regex
+import requests
 from lxml import etree
+from io import StringIO
 
+def parse_pdf_from_server(pdf_path):
+    url = 'https://kermitt2-grobid.hf.space/api/processFulltextDocument'
 
-def extract_paper_info(file_path):
+    with open(pdf_path, 'rb') as file:
+        files = {'input': file}
+        response = requests.post(url, files=files)
+
+    if response.status_code == 200:
+        xml_source = response.text
+        return xml_source
+    else:
+        print(f"Error: {response.status_code}")
+        return None
+
+def extract_paper_info(pdf_path):
+    xml_source = parse_pdf_from_server(pdf_path)
+    if xml_source is None:
+        return
+
     parser = etree.XMLParser(remove_blank_text=True)
-    tree = etree.parse(file_path, parser=parser)
-    root = tree.getroot()
+    root = etree.fromstring(xml_source.encode('utf-8'), parser=parser)  # use fromstring here
 
     ns = {"tei": "http://www.tei-c.org/ns/1.0"}
 
@@ -14,7 +32,7 @@ def extract_paper_info(file_path):
     abstract = root.xpath("//tei:profileDesc/tei:abstract/tei:div/tei:p", namespaces=ns)
     divs = root.xpath("//tei:body/tei:div", namespaces=ns)
 
-    output_file_path = f"{os.path.splitext(file_path)[0]}_extracted.txt"
+    output_file_path = f"{os.path.splitext(pdf_path)[0]}_extracted.txt"
     with open(output_file_path, 'w') as output_file:
         output_file.write("Title(s):\n")
         for title in titles:
@@ -51,4 +69,6 @@ def extract_paper_info(file_path):
 
     print(f"Extraction completed, results saved in {output_file_path}")
 
-extract_paper_info("chapter3.pdf.tei.xml")
+
+# print(parse_pdf_from_server("Gratitude.pdf"))
+extract_paper_info("Gratitude.pdf")
